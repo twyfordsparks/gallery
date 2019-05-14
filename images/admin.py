d3f081e1d11e4ee1fa2c43335d13cd332b2b2d2d
john victor <johnvictor0002@gmail.com>
2019-05-14 12:32:05 +0300

    added the admin page which contains all categories,images and the settings to the admin page

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Editor, Images, tags,Category,Location
 
-# Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+admin.site.register(Editor)
+admin.site.register(Images,ImageAdmin)
+admin.site.register(tags)
+admin.site.register(Category)
+admin.site.register(Location)
